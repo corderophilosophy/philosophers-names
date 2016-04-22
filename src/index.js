71e7127891ffae1236f12b542e749362b787392e
Rich Cordero <rich@corderophilosophy.com>
2016-04-22 11:54:36 -0400

    feat(random): Add ability to get an array of philosopher names
    If you pass a number to the random function, you will get an array with that number of items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const uniqueRandomArray = require('unique-random-array');
 const philosopherNames = require('./philosophers.json');
+const getRandomItem = uniqueRandomArray(philosopherNames);
 
 module.exports = {
   all: philosopherNames,
-  random: uniqueRandomArray(philosopherNames)
+  random: random
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < number; i += 1) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
